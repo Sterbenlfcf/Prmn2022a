9dca5880bf53279757d61e12c89e350514e4af50
Sterbenlfcf <b2210350@photon.chitose.ac.jp>
2022-11-01 21:04:03 +0900

    課題1-2作成

---
+++ prmn2022a/src/lecture01/Kadai2.java
@@ -0,0 +1,11 @@
+package lecture01;
+
+public class Kadai2 {
+    public static void main(String[] args) {
+
+        int studentNumber = 2210350;
+
+        System.out.println("b" + studentNumber +" Hiroya Iwasaki");
+
+    }
+}
