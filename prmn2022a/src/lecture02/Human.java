e1dc52578fa8a8b8491a22ff021ed5f30cfbf95f
Sterbenlfcf <b2210350@photon.chitose.ac.jp>
2022-11-04 23:07:49 +0900

    課題2-1作成

---
+++ prmn2022a/src/lecture02/Human.java
@@ -0,0 +1,24 @@
+package lecture02;
+
+public class Human {
+
+    String name;
+    int age;
+
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void print(){
+
+        if(this.age <= 18){
+            System.out.print("生徒:");
+        }else if(this.age <= 22){
+            System.out.print("学生:");
+        }
+        System.out.println(this.name + " " + this.age + "歳です。");
+
+    }
+
+}
