71047826f8dfafaa40bf50fdca1c99f067c41ab0
Sterbenlfcf <b2210350@photon.chitose.ac.jp>
2022-11-06 15:45:10 +0900

    課題2-2作成

---
+++ prmn2022a/src/lecture02/Exercise2_2.java
@@ -0,0 +1,14 @@
+package lecture02;
+
+public class Exercise2_2 {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        GasStation gs = new GasStation();
+
+        car.run();
+        gs.refuel(car);
+        car.run();
+
+    }
+}
