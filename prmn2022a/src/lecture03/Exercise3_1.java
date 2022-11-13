17b8a16d3f0fb19375cefd4db2a59d3ddc9ad64e
Sterbenlfcf <b2210350@photon.chitose.ac.jp>
2022-11-13 19:56:45 +0900

    課題3-1作成

---
+++ prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,13 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("任意の文字列を乳直してください：");
+        String input = scanner.nextLine();
+        System.out.println("\"" + input + "\" と入力されました");
+    }
+}
