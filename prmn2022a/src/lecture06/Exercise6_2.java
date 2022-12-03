1870f253aa053d49c55a595cbf7463aabb68fb96
Sterbenlfcf <b2210350@photon.chitose.ac.jp>
2022-12-04 03:19:54 +0900

    課題6-2作成

---
+++ prmn2022a/src/lecture06/Exercise6_2.java
@@ -0,0 +1,28 @@
+package lecture06;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise6_2 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        for(int i=0; i<5; i++){
+            integers.add(random.nextInt(6) + 1);
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("さいころを5つ振りました.\n何番目のさいころの値を確認しますか?");
+        try{
+            int index = input.nextInt();
+            System.out.println(index + "番目の値は " + integers.get(index));
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("ArrayListの範囲外アクセスを確認しました.\nプログラムを終了します.");
+
+        }
+    }
+}
